4b719693601baba6ecfe10ea87f97e5d4c79f400
Drew Branum <59579859+Andrew-Branum1@users.noreply.github.com>
2025-07-31 15:38:56 -0400

    added an agent

--- langchain_helper.py
+++ langchain_helper.py
@@ -1,5 +1,8 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import GoogleGenerativeAI
+from langchain_community.agent_toolkits.load_tools import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,5 +19,14 @@ def generate_pet_name(animal_type, pet_color):
     response = name_chain.invoke({"animal_type": animal_type, "pet_color": pet_color})
     return response
 
+def langchain_agent():
+    llm= GoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.5)
+    tools = load_tools(["wikipedia","llm-math"], llm = llm)
+    agent = initialize_agent(
+        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = True
+    )
+
+    result = agent.run("What is the average age of a dog? Multiply the age by 3")
 if __name__ == "__main__":
-    print(generate_pet_name("lizard","white"))
+    #print(generate_pet_name("lizard","white"))
+    langchain_agent()
